8d501b4668e7beed1149343b9c2f7e52e1b4270d
Evtyagin Maksim <evtyagin.maksim@gmail.com>
2023-02-14 09:50:43 +0200

    Init commit

---
+++ cmd/storage/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/maxwellzp/storage/internal/storage"
+)
+
+func main() {
+	st := storage.NewStorage()
+
+	file, err := st.Upload("test.txt", []byte("hello"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	restoredFile, err := st.GetByID(file.ID)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+
+	fmt.Println("it uploaded", restoredFile)
+}
